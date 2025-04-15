19226313b05716d21cb89055d15eb6244fb07034
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-15 17:23:03 +0300

    Added constructor to AFeature class

--- src/ro/ase/cts/practice/models/factory/AFeature.java
+++ src/ro/ase/cts/practice/models/factory/AFeature.java
@@ -3,4 +3,9 @@ package ro.ase.cts.practice.models.factory;
 public abstract class AFeature {
     String name;
     int price;
+
+    public AFeature(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
