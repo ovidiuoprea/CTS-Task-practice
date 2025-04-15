bce7edc788af4beca15902ecd9966946d3ccc790
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-15 17:32:53 +0300

    Tested Feature Factory Method

--- src/ro/ase/cts/practice/Main.java
+++ src/ro/ase/cts/practice/Main.java
@@ -1,7 +1,23 @@
 package ro.ase.cts.practice;
 
+import ro.ase.cts.practice.enums.FeatureType;
+import ro.ase.cts.practice.models.factory.AFeature;
+import ro.ase.cts.practice.models.factory.FeatureFactoryMethod;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        FeatureFactoryMethod featureFactory = new FeatureFactoryMethod();
+
+        AFeature antenna = featureFactory.getFeature(FeatureType.ANTENNA, "Antenna", 50);
+        AFeature battery = featureFactory.getFeature(FeatureType.BATTERY, "Battery", 100);
+        AFeature compass = featureFactory.getFeature(FeatureType.COMPASS, "Compass", 150);
+        AFeature speaker = featureFactory.getFeature(FeatureType.SPEAKER, "Speaker", 200);
+
+
+        System.out.println(antenna);
+        System.out.println(battery);
+        System.out.println(compass);
+        System.out.println(speaker);
     }
 }
