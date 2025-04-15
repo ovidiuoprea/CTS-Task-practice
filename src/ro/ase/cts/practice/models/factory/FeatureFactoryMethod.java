488e4984b9d077d0a024de843dbcc74b338ce39a
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-15 17:26:16 +0300

    Created concrete Feature factory class

---
+++ src/ro/ase/cts/practice/models/factory/FeatureFactoryMethod.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.practice.models.factory;
+
+import ro.ase.cts.practice.enums.FeatureType;
+
+public class FeatureFactoryMethod implements IFeatureFactory {
+    @Override
+    public AFeature getFeature(FeatureType feature, String name, int price) {
+        return null;
+    }
+}
